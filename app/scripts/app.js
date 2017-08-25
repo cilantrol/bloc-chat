2ae7468934fbfe1c3dbcda6aeba7da34dea9b414
cilantrol <gaowhui@gmail.com>
2017-08-25 16:50:51 -0700

    setup complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,21 @@
 (function() {
-    function config($locationProvider, $stateProvider)  {
-        $locationProvider
-            .html5Mode({
-                enabled: true,
-                requireBase: false
-          });
-        $stateProvider
-            .state('home',  {
-                url: '/',
-                controller: 'HomeCtrl as home',
-                templateUrl: '/templates/home.html'
-            });
-    }
+  function config($locationProvider, $stateProvider)  {
+      $locationProvider
+        .html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+
+      $stateProvider
+        .state('home',  {
+            url: '/',
+            controller: 'HomeCtrl as home',
+            templateUrl: '/templates/home.html'
+        });
+  }
 
 
   angular
-      .module('blocChat', ['ui.router', 'firebase']);
+    .module('blocChat', ['ui.router', 'firebase'])
+    .config(config);
 })();
